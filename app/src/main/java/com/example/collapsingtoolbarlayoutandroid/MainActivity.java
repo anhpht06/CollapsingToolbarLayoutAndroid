cb0c4eeeaf3fa0b44481cf59b9675adfcc5ef2d8
tuanichi <tuanichi06@gmail.com>
2020-06-03 16:05:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/collapsingtoolbarlayoutandroid/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.collapsingtoolbarlayoutandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         toolbar =  findViewById(R.id.toolbar);
+         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+
+}
